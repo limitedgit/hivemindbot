3f29711549534a9b925b547a1ba344847519cf9b
limitedgit <jameswang0505@hotmail.com>
2018-04-11 20:57:21 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ client.on('ready', () => {
 });
 
 client.on("message", async message => {
+    
+    const args = message.content.slice("!".length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
     if (message.content === 'hi') {
     	message.reply('hi');
   	}
+    if(command === "say") {
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
