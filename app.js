a9a5d9e7c69a360d4ce7c5ead90e1f9601dd226c
limitedgit <jameswang0505@hotmail.com>
2018-04-11 20:18:23 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,54 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+   
+    if(message.author.bot) return;
+
+    if(message.content.indexOf("+") !== 0) return;
+
+  const args = message.content.slice("+".length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+    
+  
+  if(command === "say") {
+
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  }
+  
+  if(command === "kick") {
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("That is not a current member");
+    if(!member.kickable) 
+      return message.reply("I do not have the permissions required.");
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+  }
+  
+  if(command === "purge") {
+
+    const deleteCount = parseInt(args[0], 10);  
+
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+    
 });
+
 
 client.login(process.env.BOT_TOKEN);
