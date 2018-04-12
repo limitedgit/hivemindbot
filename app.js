d1af3beb7bcf6f0c0c797ec39c75f215e422451c
limitedgit <jameswang0505@hotmail.com>
2018-04-11 21:05:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ client.on("message", async message => {
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
+    }
     if(command === "purge") {
     const deleteCount = parseInt(args[0], 10);
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
@@ -27,8 +28,7 @@ client.on("message", async message => {
     const fetched = await message.channel.fetchMessages({count: deleteCount});
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
-  }
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
