605e898b672af81c50df6d5f9abe82519ebcde43
limitedgit <jameswang0505@hotmail.com>
2018-04-11 19:43:15 -0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,33 @@
+import discord
+import asyncio
+import random
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+
+    if message.content.startswith('bot denounce someone'):
+        await client.send_message(message.channel, 'Type $name namehere')
+        def check(msg):
+            return msg.content.startswith('$name')
+        message = await client.wait_for_message(author=message.author, check=check)
+        name = message.content[len('$name'):].strip()
+        await client.send_message(message.channel, '{} is a noob'.format(name))
+    elif message.content.startswith('$react'):
+        msg = await client.send_message(message.channel, ':ok_hand:')
+        res = await client.wait_for_reaction(':ok_hand:')
+        res = await client.wait_for_reaction(message=msg, check=check)
+        await client.send_message(message.channel, '{0.user} reacted with {0.reaction.emoji}!'.format(res))
+ 
+
+
+
+client.run('BOT_TOKEN', bot=True)
