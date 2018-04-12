bedef1c1a95befada8eca931d1ee9287cfaa64e8
limitedgit <jameswang0505@hotmail.com>
2018-04-11 20:22:16 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,14 +7,10 @@ client.on('ready', () => {
 
 client.on('message', message => {
    
-   if(message.author.bot) return;
+  if(message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-
-     if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
     
   
   if(command === "say") {
