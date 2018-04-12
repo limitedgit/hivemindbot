4104c190c431a09cc2785c3a061374df5cac624c
limitedgit <jameswang0505@hotmail.com>
2018-04-11 21:02:08 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('My Body is ready!');
 });
 
 client.on("message", async message => {
@@ -20,6 +20,14 @@ client.on("message", async message => {
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
+    if(command === "purge") {
+    const deleteCount = parseInt(args[0], 10);
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("2 - 100 please");
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
   }
 });
 
