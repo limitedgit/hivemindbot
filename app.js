308a491a0eb659edec7006ecdfaf1d177f0e9da6
limitedgit <jameswang0505@hotmail.com>
2018-04-12 20:42:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -30,9 +30,6 @@ client.on("message", async message => {
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
     }
-    if (command === "server"`) {
-        message.reply("This server's name is: ${message.guild.name}");
-    }  
 });
 
 client.login(process.env.BOT_TOKEN);
