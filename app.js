6a525f77613d2b6c9811c97841135bdc9b93919d
limitedgit <jameswang0505@hotmail.com>
2018-10-27 17:53:40 -0600

    Update app.js

--- app.js
+++ app.js
@@ -22,12 +22,13 @@ client.on("message", async message => {
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
     }
+    if (command === "server"){
+    message.channel.send("welcome to ${message.guild.name}")
+    }
     if(command === "purge") {
     const deleteCount = parseInt(args[0], 10);
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100){
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.reply("2 - 100 please");
-    }
-    else{
     const fetched = await message.channel.fetchMessages({count: deleteCount});
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
